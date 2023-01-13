20f59f9963cbb332d78b7766d75e96b1b475d6fc
TheBigH50 <HFangmeier@outlook.com>
2023-01-13 11:08:57 -0600

    write a function that finds the middle digit of an array triplet without sort

--- sandbox.js
+++ sandbox.js
@@ -519,3 +519,90 @@ checkTriangle(2, 2, 2); //equilateral
 checkTriangle(4, 5, 6); //scalene
 checkTriangle(10, 5, 7); //scalene
 checkTriangle(1, 1, 10); //invalid
+
+function gimme(triplet) {
+  let ans = 0;
+  let sorted = [...triplet];
+
+  sorted.sort((a, b) => a - b);
+
+  triplet.forEach((a, b) => {
+    if (a == sorted[1]) {
+      ans = b;
+    }
+  });
+  return ans;
+}
+
+function gimme(triplet) {
+  let ans = 2;
+  let a = triplet[0];
+  let b = triplet[1];
+  let c = triplet[2];
+
+  if (a < 0 && b < 0 && c < 0) {
+    a = a * -1;
+    b = b * -1;
+    c = c * -1;
+  }
+
+  if (a + b > a + c && b + c < a + b) {
+    if (a > b) {
+      ans = 1;
+    } else {
+      ans = 0;
+    }
+  } else if (b + c > a + c) {
+    if (b < c) {
+      ans = 1;
+    } else {
+      ans = 2;
+    }
+  } else if (a + b < b + c) {
+    if (a < c) {
+      ans = 0;
+    } else {
+      ans = 2;
+    }
+  }
+
+  if (a < 0 && b < 0) {
+    if (a > b) {
+      ans = 0;
+    } else {
+      ans = 1;
+    }
+  } else if (a < 0 && c < 0) {
+    if (a > c) {
+      ans = 0;
+    } else {
+      ans = 2;
+    }
+  } else if (b < 0 && c < 0) {
+    if (b > c) {
+      ans = 1;
+    } else {
+      ans = 2;
+    }
+  }
+  if (a < 0 && b > 0 && c > 0) {
+    if (b > c) {
+      ans = 2;
+    } else {
+      ans = 1;
+    }
+  } else if (b < 0 && a > 0 && c > 0) {
+    if (a > c) {
+      ans = 2;
+    } else {
+      ans = 0;
+    }
+  } else if (c < 0 && a > 0 && b > 0) {
+    if (a > b) {
+      ans = 1;
+    } else {
+      ans = 0;
+    }
+  }
+  return ans;
+}
