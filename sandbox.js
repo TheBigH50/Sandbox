583c6fd5105bc16de8944bf93842646425d133c8
TheBigH50 <HFangmeier@outlook.com>
2023-01-16 09:43:30 -0600

    start making deck shuffler

--- sandbox.js
+++ sandbox.js
@@ -726,3 +726,47 @@ function gimme(triplet) {
 }
 
 console.log(gimme(trips));
+
+function correct(string) {
+  let ans = [];
+  let processed = string.split("");
+
+  processed.forEach((a) => {
+    if (a === "5") {
+      ans.push("S");
+    } else if (a === "0") {
+      ans.push("O");
+    } else if (a === "1") {
+      ans.push("I");
+    } else {
+      ans.push(a);
+    }
+  });
+  return ans.join("");
+}
+
+
+let deck = [
+  "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+  "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+  "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+  "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"
+  ];
+let shuffledDeck = [];
+
+function shuffle(deck) {
+  let tempDeck = [];
+  while (tempDeck.length < 52) {
+    let a = Math.floor(Math.random() * (52 - 1) + 1);
+    let b = Math.floor(Math.random() * (52 - 1) + 1);
+    let c = Math.floor(Math.random() * (52 - 1) + 1);
+    let d = a + b + c;
+
+    if (tempDeck.includes(d) == false && d > 52 && d < 105) {
+      tempDeck.push(d);
+    }
+  }
+  console.log(tempDeck);
+}
+
+shuffle();
